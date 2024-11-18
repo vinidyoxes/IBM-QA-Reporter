4cd8a1c20b848bdfb1f032c628c5890438f2de78
Vinicius Ortega <Vinicius.Ortega@ibm.com>
2024-11-18 17:28:14 -0300

    remove item on reported list

--- qaReporter.js
+++ qaReporter.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         QA Reporter 
-// @version      1.0.2
+// @version      1.0.3
 // @description  Just a tool for doing reports!
 // @author       Vinicius Ortega 
 // @match        https://www.ibm.com/*
@@ -17,7 +17,7 @@
 
     let listSection;
     let reportSection;
-    const version = "1.0.2";
+    const version = "1.0.3";
 
     const Reporter = () => {
         
@@ -29,7 +29,7 @@
             <span style="font-size:14px">v${version}</span>
             <div class="floatinIcons">
             <i data-function="list" class='switcher' title="Current List" >
-               <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+               <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/>
                 </svg>
 
@@ -270,6 +270,7 @@
         .removeIcon{
             cursor:pointer;
             user-select: none;
+            color:#5C5D5E;
         }
         .removeIcon:hover{
             color:#0050E6;
